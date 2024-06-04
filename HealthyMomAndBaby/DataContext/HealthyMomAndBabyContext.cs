5de81e0cf34fef4fb3763cb0f208eeecd270a324
HienKhoa <Khoathfpt@gmail.com>
2024-06-05 02:03:54 +0700

    HMAB-01-Setup EFCore

---
+++ HealthyMomAndBaby/DataContext/HealthyMomAndBabyContext.cs
@@ -0,0 +1,13 @@
+﻿using HealthyMomAndBaby.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthyMomAndBaby.DataContext
+{
+    public class HealthyMomAndBabyContext : DbContext
+    {
+        public HealthyMomAndBabyContext(DbContextOptions<HealthyMomAndBabyContext> options) : base(options)
+        {
+        }
+
+    }
+}
